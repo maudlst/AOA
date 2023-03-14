fc77116c7eafa5714cc7f28f67ecaf8c4128adb1
ahenches <arnaud.henches@gmail.com>
2023-03-14 22:24:40 +0100

    driver etalonnage

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ CFLAGS=-O2 -g -Wall
 OPTFLAGS=-O3 -g -Wall
 OBJS_COMMON= kernel.o rdtsc.o
 
-all:	base	measure
+all:	base	measure	etalonnage
 
 
 base:  $(OBJS_COMMON) driver_base.o
@@ -12,6 +12,8 @@ base:  $(OBJS_COMMON) driver_base.o
 measure: $(OBJS_COMMON) driver.o
 	$(CC) -o $@ $^
 
+etalonnage: $(OBJS_COMMON) driver_etalonnage.o
+	$(CC) -o $@ $^
 
 driver_base.o: driver_base.c
 	$(CC) $(CFLAGS) -c $<
@@ -19,6 +21,8 @@ driver_base.o: driver_base.c
 driver.o: driver.c
 	$(CC) $(CFLAGS) -c $<
 
+driver_etalonnage.o: driver_etalonnage.c
+	$(CC) $(CFLAGS) -c $<
 
 clean:
 	rm -rf $(OBJS_COMMON) driver.o driver_base.o measure base
---
+++ driver_etalonnage.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h> // atoi, qsort
+#include <stdint.h>
+#include <unistd.h>
+
+#define NB_METAS 31
+
+
+
+extern uint64_t rdtsc();
+extern void kernel(unsigned n, float a[n], const float b[n]);
+extern void init_array(int n, float b[n]);
+
+
+static int cmp_uint64 (const void *a, const void *b){
+    const uint64_t va = *((uint64_t *) a);
+    const uint64_t vb = *((uint64_t *) b);
+
+    if (va < vb) return -1;
+    if (va > vb) return 1;
+    return 0;
+}
+
+
+int main(int argc, char *argv[])
+{
+    /* check command line arguments */
+    if (argc != 5)
+    {
+        fprintf(stderr, "Usage: %s <size> <output file name> <nombre de warmups> <nombre de repetitions pendant la mesure>\n", argv[0]);
+        return 1;
+    }
+
+    /* get command line arguments */
+    const unsigned size = atoi(argv[1]); /* problem size */
+    //const char *output_file_name = argv[2];
+
+   
+
+    // warmup
+    int nb_repm = atoi(argv[4]);
+    //int nb_repw = atoi(argv[3]);
+    uint64_t temps_diff[nb_repm][NB_METAS];
+
+    for (int m = 0; m < NB_METAS; m++)
+    {
+
+         /* allocate arrays.  */
+        float *a = malloc(size * sizeof a[0]);
+        float *b = malloc(size * sizeof b[0]);
+
+        /* init arrays */
+        srand(0);
+        init_array(size, b);
+
+        for (int j = 0; j < nb_repm; j++)
+        {
+            uint64_t temps_deb = rdtsc();
+            kernel(size, a, b);
+            uint64_t temps_fin = rdtsc();
+
+            temps_diff[j][m] = temps_fin - temps_deb;
+        }
+
+        /* free arrays.  */
+        free(a);
+        free(b);
+
+        sleep(3);
+
+    }
+
+    for(int j = 0; j < nb_repm; j++)
+    {
+        const unsigned nb_iters = size * nb_repm;
+        qsort(temps_diff[j], NB_METAS, sizeof temps_diff[j][0], cmp_uint64);
+        printf("Minimum %lu RDTSC-cycles (%.2f per iner-iter)\n", 
+                temps_diff[j][0], (float)temps_diff[j][0] / nb_iters);
+        printf("Mediane %lu RDTSC-cycles (%.2f per iner-iter)\n", 
+                temps_diff[j][NB_METAS/2], (float)temps_diff[j][NB_METAS/2] / nb_iters);
+        const float stab = (temps_diff[j][NB_METAS/2] - temps_diff[j][0]) * 100.0f / temps_diff[j][0];
+
+        if(stab >= 10)
+        {
+            printf("Mauvaise stabilité : %.2f %%\n", stab);
+        }
+        else if (stab >= 5)
+        {
+            printf("Moyenne stabilité : %.2f %%\n", stab);
+        }
+        else
+        {
+            printf("Bonne stabilité : %.2f %%\n", stab);
+        }
+    }
+
+
+
+    return EXIT_SUCCESS;
+}
