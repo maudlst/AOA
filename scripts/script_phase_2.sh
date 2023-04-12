29bae1eb3ac90760a9b1e673c393a68923a75fe2
ahenches <arnaud.henches@gmail.com>
2023-04-12 17:47:22 +0200

    ajout du choix du kernel dans le script

--- scripts/script_phase_2.sh
+++ scripts/script_phase_2.sh
@@ -1,14 +1,14 @@
-if [ $# -eq 3 ]
+if [ $# -eq 4 ]
 then
     echo "Compilation et maqao dans tmp/"
 else 
-    echo "Mauvais nombre d'arguments"
+    echo "Mauvais nombre d'arguments. Utilisation : <nom_fichier_kernel> <n> <warmup> <n_rep> !"
     exit
 fi 
 
 cd src/
 
-make CC=gcc OPTFLAGS=-O2
+make CC=gcc OPTFLAGS=-O2 KERNEL=$1
 
-maqao oneview -R1 -xp=../tmp --replace -- ./measure $1 $2 $3  
+maqao oneview -R1 -xp=../tmp --replace -- ./measure $2 $3 $4 
 
