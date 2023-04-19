b4a84cce4c91e5778883e3cdfe6b85848ee3f7d8
ahenches <arnaud.henches@gmail.com>
2023-04-19 17:38:50 +0200

    update script

--- scripts/script_phase_2.sh
+++ scripts/script_phase_2.sh
@@ -1,17 +1,19 @@
-if [ $# -eq 4 ]
+if [ $# -eq 5 ]
 then
     dir=$1
+    kernel=$2
     echo "Compilation et maqao dans tmp/"
 else 
-    echo "Mauvais nombre d'arguments. Utilisation : <nom_fichier_kernel> <n> <warmup> <n_rep> !"
+    echo "Mauvais nombre d'arguments. Utilisation : <cache> <nom_fichier_kernel> <n> <warmup> <n_rep> !"
     exit
 fi 
 
 cd src/
 
-make CC=gcc OPTFLAGS=-O2 KERNEL=$1.c
+make CC=gcc OPTFLAGS=-O2 KERNEL=$kernel.c
 
-maqao oneview -R1 -xp=../tmp --replace -- ./measure $2 $3 $4 
+maqao oneview -R1 -xp=../tmp --replace -- ./measure $3 $4 $5
 
-rm -rf ../phase_2_html/$dir/
-cp -r ../tmp/RESULTS/measure_one_html/ ../phase_2_html/$dir
+rm -rf ../phase_2_html/$dir/$kernel/
+cp -r ../tmp/RESULTS/measure_one_html/ ../phase_2_html/$dir/$kernel
+
