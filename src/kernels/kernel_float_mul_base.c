c693e97031373f2dd366bc4337e99b64e80197f2
ahenches <arnaud.henches@gmail.com>
2023-04-30 13:37:58 +0200

    ajout d'un dossier kernel

---
+++ src/kernels/kernel_float_mul_base.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void kernel(unsigned n, float a[n], const float b[n])
+{
+    unsigned i;
+    float s = 0.0f;
+    float *tmp = malloc(n * sizeof(*tmp));
+    for (i = 0; i < n; i++)
+    {
+        tmp[i] = i + b[i];
+        s += tmp[i];
+    }
+    const float inv_s = 1 / s;
+    for (i = 0; i < n; i++)
+        a[i] = tmp[i] * inv_s;
+    free(tmp);
+}
+
+void init_array(int n, float b[n])
+{
+    int i;
+
+    for (i = 0; i < n; i++)
+        b[i] = (float)rand() / RAND_MAX;
+}
+
+void print_array(int n, float a[n], const char *output_file_name)
+{
+    int i;
+
+    FILE *fp = fopen(output_file_name, "w");
+    if (fp == NULL)
+    {
+        fprintf(stderr, "Cannot write to %s\n", output_file_name);
+        return;
+    }
+
+    for (i = 0; i < n; i++)
+        fprintf(fp, "%f\n", a[i]);
+
+    fclose(fp);
+}
